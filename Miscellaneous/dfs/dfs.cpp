59ae37ec54e646b6e50f44eb9a7edd2d4098c811
suri321 <sharmasuri3@gmail.com>
2020-05-22 10:38:30 +0530

    dfs updated

--- Miscellaneous/dfs/dfs.cpp
+++ Miscellaneous/dfs/dfs.cpp
@@ -4,14 +4,12 @@ vector<int> adj[N];
  
 void numberOfNodes(int s, int e) 
 { 
-    vector<int>::iterator u; 
     count1[s] = 1; 
-    for (u = adj[s].begin(); u != adj[s].end(); u++) { 
-        if (*u == e) 
-            continue; 
-        numberOfNodes(*u, s); 
-        count1[s] += count1[*u]; 
-    } 
+    for(int i=0;i<adj[s].size();i++){
+    	if(adj[s][i]==e)continue;
+    	numberOfNodes(adj[s][i],s);
+    	count1[s]+=count1[adj[s][i]];
+	}
 } 
 
 numberOfNodes(1, 0);
